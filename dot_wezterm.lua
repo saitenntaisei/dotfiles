ee59a9b040fb87501e630ae4e48faaf240247f40
saitenntaisei <saitenntaisei@gmail.com>
2024-08-26 08:49:20 +0900

    update wezterm setting

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -48,6 +48,7 @@ if wezterm.config_builder then
 
   -- キーバインドの設定
   local act = wezterm.action
+
   config.keys = {
     {
       key = 'd',
@@ -93,6 +94,25 @@ if wezterm.config_builder then
       action = act.CloseCurrentPane { confirm = true },
     },
     { key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
+    {
+      key = 'h',
+      mods = 'CTRL',
+      action = act.AdjustPaneSize { 'Left', 5 },
+    },
+    {
+      key = 'j',
+      mods = 'CTRL',
+      action = act.AdjustPaneSize { 'Down', 5 },
+    },
+    { key = 'k',
+      mods = 'CTRL',
+      action = act.AdjustPaneSize { 'Up', 5 },
+    },
+    {
+      key = 'l',
+      mods = 'CTRL',
+      action = act.AdjustPaneSize { 'Right', 5 },
+    },
   }
 
   config.window_close_confirmation = 'NeverPrompt'
