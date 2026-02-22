f5c6b386f28cb4b75c546fed2cc7bb267f995128
saitenntaisei <saitenntaisei@gmail.com>
2026-02-22 21:03:04 +0900

    feat: new config

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -13,11 +13,12 @@ if wezterm.config_builder then
   config.font = wezterm.font("NotoMono Nerd Font", {weight="Regular", stretch="Normal", style="Normal"})
   -- フォントサイズの設定
   config.font_size = 13
+  config.macos_window_background_blur = 20
   config.background = {
     -- This is the deepest/back-most layer. It will be rendered first
     {
       source = {
-        File = '/home/saitenntaisei/Pictures/miko.jpg',
+        File = '/Users/saitenntaisei/Pictures/miko.jpg',
       },
       -- The texture tiles vertically but not horizontally.
       -- When we repeat it, mirror it so that it appears "more seamless".
